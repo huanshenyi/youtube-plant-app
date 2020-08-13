8db4a4917f715d3842bcb2cea571b10da8b5541f
huanshenyi <txy1226052@gmail.com>
2020-08-13 21:12:35 +0900

    タイトル欄の追加

--- README.md
+++ README.md
@@ -1,16 +1,7 @@
 # plant_app
 
-A new Flutter application.
+YouTubeの動画を参考して書いてみたもの
+https://www.youtube.com/watch?v=LN668OAUrK4&list=PL7k-SS-7DM-xEkKIzefqn1lZsew_wFdoD&index=2&t=353s
 
-## Getting Started
 
-This project is a starting point for a Flutter application.
 
-A few resources to get you started if this is your first Flutter project:
-
-- [Lab: Write your first Flutter app](https://flutter.dev/docs/get-started/codelab)
-- [Cookbook: Useful Flutter samples](https://flutter.dev/docs/cookbook)
-
-For help getting started with Flutter, view our
-[online documentation](https://flutter.dev/docs), which offers tutorials,
-samples, guidance on mobile development, and a full API reference.
--- lib/screens/home/components/body.dart
+++ lib/screens/home/components/body.dart
@@ -6,69 +6,99 @@ class Body extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     Size size = MediaQuery.of(context).size;
-    return Column(
-      children: [
-        Container(
-          height: size.height * 0.2,
-          child: Stack(
-            children: [
-              Container(
-                height: size.height * 0.2 - 27,
-                decoration: BoxDecoration(
-                  color: kPrimaryColor,
-                  borderRadius: BorderRadius.only(
-                    bottomRight: Radius.circular(36),
-                    bottomLeft: Radius.circular(36),
-                  ),
-                ),
+    return SingleChildScrollView(
+      child: Column(
+        children: [
+          HeaderWithSearchBox(size: size)
+        ],
+      ),
+    );
+  }
+}
+
+class HeaderWithSearchBox extends StatelessWidget {
+  const HeaderWithSearchBox({
+    Key key,
+    @required this.size
+  }):super (key: key);
+  final Size size;
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return Container(
+      height: size.height * 0.2,
+      child: Stack(
+        children: [
+          Container(
+            padding: EdgeInsets.only(
+              left: kDefaultPadding,
+              right: kDefaultPadding,
+              bottom: 36 + kDefaultPadding,
+            ),
+            height: size.height * 0.2 - 27,
+            decoration: BoxDecoration(
+              color: kPrimaryColor,
+              borderRadius: BorderRadius.only(
+                bottomRight: Radius.circular(36),
+                bottomLeft: Radius.circular(36),
               ),
-              Positioned( //ヘットのインプット
-                  bottom: 0,
-                  left: 0,
-                  right: 0,
-                  child: Container(
-                    alignment: Alignment.center,
-                    margin: EdgeInsets.symmetric(horizontal: kDefaultPadding),
-                    padding: EdgeInsets.symmetric(horizontal: kDefaultPadding),
-                    height: 54,
-                    decoration: BoxDecoration(
-                        color: Colors.white,
-                        borderRadius: BorderRadius.circular(20),
-                        boxShadow: [
-                          BoxShadow(
-                            offset: Offset(0, 10),
-                            blurRadius: 50,
-                            color: kPrimaryColor.withOpacity(0.23),
-                          )
-                        ],
-                    ),
-                    child: Row(
-                      children: [
-                        Expanded(
-                            child: TextField(
-                              onChanged: (value) {
-                                print(value);
-                              },
-                              decoration: InputDecoration(
-                                hintText: "Search",
-                                hintStyle: TextStyle(
-                                  color: kPrimaryColor.withOpacity(0.5),
-                                ),
-                                enabledBorder: InputBorder.none,// テキスト入力のラインを消す
-                                focusedBorder: InputBorder.none,
-                                // suffixIcon: SvgPicture.asset("assetcs/icons/search.svg")
-                              ),
+            ),
+            child: Row(children: [
+              Text('Hi Uishopy!', style: Theme.of(context).textTheme.headline5.copyWith(
+                  color: Colors.white,
+                  fontWeight: FontWeight.bold),
+              ),
+              Spacer(),
+              Image.asset("assetcs/images/logo.png")
+            ],
+            ),
+          ),
+          Positioned( //ヘットのインプット
+              bottom: 0,
+              left: 0,
+              right: 0,
+              child: Container(
+                  alignment: Alignment.center,
+                  margin: EdgeInsets.symmetric(horizontal: kDefaultPadding),
+                  padding: EdgeInsets.symmetric(horizontal: kDefaultPadding),
+                  height: 54,
+                  decoration: BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.circular(20),
+                    boxShadow: [
+                      BoxShadow(
+                        offset: Offset(0, 10),
+                        blurRadius: 50,
+                        color: kPrimaryColor.withOpacity(0.23),
+                      )
+                    ],
+                  ),
+                  child: Row(
+                    children: [
+                      Expanded(
+                        child: TextField(
+                          onChanged: (value) {
+                            print(value);
+                          },
+                          decoration: InputDecoration(
+                            hintText: "Search",
+                            hintStyle: TextStyle(
+                              color: kPrimaryColor.withOpacity(0.5),
                             ),
+                            enabledBorder: InputBorder.none,// テキスト入力のラインを消す
+                            focusedBorder: InputBorder.none,
+                            // suffixIcon: SvgPicture.asset("assetcs/icons/search.svg")
+                          ),
                         ),
-                        SvgPicture.asset("assetcs/icons/search.svg"),
-                      ],
-                    )
+                      ),
+                      SvgPicture.asset("assetcs/icons/search.svg"),
+                    ],
                   )
               )
-            ],
-          ),
-        )
-      ],
+          )
+        ],
+      ),
     );
   }
 }
