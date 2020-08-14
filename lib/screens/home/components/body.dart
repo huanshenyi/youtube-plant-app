0f1974232b1457d6665aa53042595c64fbf35ba7
huanshenyi <txy1226052@gmail.com>
2020-08-14 22:37:53 +0900

    ボタンとタイトルの移動

--- lib/screens/home/components/body.dart
+++ lib/screens/home/components/body.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:plant_app/constants.dart';
-
+import 'package:plant_app/screens/home/components/title_with_more_bbtn.dart';
 import 'header_with_seachbox.dart';
 
 class Body extends StatelessWidget {
@@ -12,63 +11,14 @@ class Body extends StatelessWidget {
       child: Column(
         children: [
           HeaderWithSearchBox(size: size),
-          Padding(
-              padding: const EdgeInsets.symmetric(horizontal: kDefaultPadding),
-              child: Row(
-                children: [
-                  TitleWithCustomUnderline(text: "Recomended",),
-                  Spacer(),
-                  FlatButton(
-                      shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(20),
-                      ),
-                      color: kPrimaryColor,
-                      onPressed: (){},
-                      child: Text("More", style: TextStyle(color: Colors.white),)
-                  )
-                ],
-              ),
-          )
-        ],
-      ),
-    );
-  }
-}
-
-class TitleWithCustomUnderline extends StatelessWidget {
-  const TitleWithCustomUnderline({
-    Key key,
-    this.text,
-  }): super(key: key);
-
-  final String text;
-
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      height: 24,
-      child: Stack(
-        children: [
-          Padding(
-            padding: const EdgeInsets.only(left: kDefaultPadding / 4),
-            child: Text(
-              text,
-              style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
-            ),
-          ),
-          Positioned(
-              bottom: 0,
-              left: 0,
-              right: 0,
-              child: Container(
-                margin: EdgeInsets.only(right: kDefaultPadding / 4),
-                height: 7,
-                color: kPrimaryColor.withOpacity(0.2),
-              )//透明度
+          TitleWithMoreBtn(
+            title: "Recomened",
+            press: (){},
           ),
         ],
       ),
     );
   }
 }
+
 
---
+++ lib/screens/home/components/title_with_more_bbtn.dart
@@ -0,0 +1,71 @@
+import 'package:flutter/material.dart';
+import 'package:plant_app/constants.dart';
+
+class TitleWithMoreBtn extends StatelessWidget {
+
+  const TitleWithMoreBtn({
+    Key key,
+    this.title,
+    this.press,
+  }): super(key:key);
+  final String title;
+  final Function press;
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: kDefaultPadding),
+      child: Row(
+        children: [
+          TitleWithCustomUnderline(text: this.title,),
+          Spacer(),
+          FlatButton(
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(20),
+              ),
+              color: kPrimaryColor,
+              onPressed: this.press,
+              child: Text("More", style: TextStyle(color: Colors.white),)
+          )
+        ],
+      ),
+    );
+  }
+}
+
+class TitleWithCustomUnderline extends StatelessWidget {
+  const TitleWithCustomUnderline({
+    Key key,
+    this.text,
+  }): super(key: key);
+
+  final String text;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 24,
+      child: Stack(
+        children: [
+          Padding(
+            padding: const EdgeInsets.only(left: kDefaultPadding / 4),
+            child: Text(
+              text,
+              style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+            ),
+          ),
+          Positioned(
+              bottom: 0,
+              left: 0,
+              right: 0,
+              child: Container(
+                margin: EdgeInsets.only(right: kDefaultPadding / 4),
+                height: 7,
+                color: kPrimaryColor.withOpacity(0.2),
+              )//透明度
+          ),
+        ],
+      ),
+    );
+  }
+}
